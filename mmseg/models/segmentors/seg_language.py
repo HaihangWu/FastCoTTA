7058af5160c3088bba6dfcd4b9bb5b05276e10c9
hthh1211 <hthh1211@sina.com>
2023-04-12 01:23:22 +1000

    new

--- mmseg/models/segmentors/seg_language.py
+++ mmseg/models/segmentors/seg_language.py
@@ -134,7 +134,9 @@ class SegLanguage(EncoderDecoder):
         seg_logits_visual = self.decode_head.forward_test(x, img_metas, self.test_cfg)
         seg_logits_text = self.text_decode_head.forward_test(x, img_metas, self.test_cfg)
         gt_semantic_seg = gt_semantic_seg[:,:,:,:,-1]
-        print("size",seg_logits_text.size(),gt_semantic_seg.size(),gt_semantic_seg)
+        if seg_logits_text.dim!=gt_semantic_seg.dim:
+            print("dimension is different:",seg_logits_text.size(),gt_semantic_seg.size())
+            exit()
 
         loss_decode = self.decode_head.losses(seg_logits_visual + seg_logits_text, gt_semantic_seg)
 
