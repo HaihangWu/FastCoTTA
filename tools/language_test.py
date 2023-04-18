f739b872ef1df3a2fd9488f937719fa9528280c3
hthh1211 <hthh1211@sina.com>
2023-04-18 19:39:08 +1000

    new commit

--- tools/language_test.py
+++ tools/language_test.py
@@ -141,7 +141,7 @@ def main():
     pretrained_dict = torch.load(cfg.model.pretrained,map_location='cpu')
     #print(pretrained_dict['state_dict'].keys())
     #print("I'm printing the model",model.state_dict().keys())
-    model.load_state_dict(pretrained_dict['state_dict'],strict=False)
+    model.load_state_dict(pretrained_dict['state_dict'])
     # if hasattr(model, 'text_encoder'):
     #     model.text_encoder.init_weights()
     model.CLASSES = datasets[0].CLASSES
