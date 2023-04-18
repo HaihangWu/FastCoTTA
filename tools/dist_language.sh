57f42132e4fdcf6c4227e50af2a0c565d002f64c
hthh1211 <hthh1211@sina.com>
2023-04-18 18:06:54 +1000

    new commit

--- tools/dist_language.sh
+++ tools/dist_language.sh
@@ -7,4 +7,4 @@ GPUS=$2
 PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4)) #wuhh
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/language_test.py $CONFIG --launcher pytorch ${@:3}
+    $(dirname "$0")/language_train.py $CONFIG --launcher pytorch ${@:3}
