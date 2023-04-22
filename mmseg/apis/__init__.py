9e0d2fb1c7bd23e2547a3c81e212a739e34d508a
hthh1211 <hthh1211@sina.com>
2023-04-22 22:39:32 +1000

    new commit

--- mmseg/apis/__init__.py
+++ mmseg/apis/__init__.py
@@ -1,9 +1,9 @@
 from .inference import inference_segmentor, init_segmentor, show_result_pyplot
-from .test import multi_gpu_test, single_gpu_test
+from .test import multi_gpu_test, single_gpu_test,single_gpu_language_cotta
 from .train import get_root_logger, set_random_seed, train_segmentor,init_random_seed
 
 __all__ = [
     'get_root_logger', 'set_random_seed', 'train_segmentor', 'init_segmentor',
     'inference_segmentor', 'multi_gpu_test', 'single_gpu_test',
-    'show_result_pyplot','init_random_seed'
+    'show_result_pyplot','init_random_seed','single_gpu_language_cotta'
 ]
