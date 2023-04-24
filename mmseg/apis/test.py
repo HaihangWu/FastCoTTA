ff92955ae053789fb174cc34fa4b8fdd16a57060
hthh1211 <hthh1211@sina.com>
2023-04-24 11:49:15 +1000

    new commit

--- mmseg/apis/test.py
+++ mmseg/apis/test.py
@@ -170,10 +170,8 @@ def single_gpu_language_cotta(model,
                 img_id = 4  # The default size without flip
             # result, probs_, preds_ = anchor_model(return_loss=False, img=[data['img'][img_id]],img_metas=[data['img_metas'][img_id].data[0]])#**data)
             # mask = (torch.amax(probs_[0], 0).cpu().numpy() > 0.69).astype(np.int64)
-            if (frame_passed) < 3200:
-                result_ori, probs, preds = ema_model(return_loss=False, **data)
-            else:
-                result_ori, probs, preds = ema_model(return_loss=False, img=[data['img'][img_id]],img_metas=[data['img_metas'][img_id].data[0]])
+            result_ori, probs, preds = ema_model(return_loss=False, **data)
+
             # result = [(mask*preds[img_id][0] + (1.-mask)*result[0]).astype(np.int64)]
             #result = [preds[img_id][0].astype(np.int64)]
             result=[result_ori[0].astype(np.int64)]
