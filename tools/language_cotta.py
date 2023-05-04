8703d4a408b3edcd6eb7cfda583a93806cadc82c
hthh1211 <hthh1211@sina.com>
2023-05-04 16:43:22 +1000

    new commit

--- tools/language_cotta.py
+++ tools/language_cotta.py
@@ -204,7 +204,7 @@ def main():
             outputs,frame_passed,domains_detections,total_predict_time = single_gpu_language_cotta(model, data_loader, args.show, args.show_dir,
                                       efficient_test,anchor, ema_model, anchor_model,frame_passed,domains_detections,total_predict_time, i*4+j)
 
-            if j % 2 != 0:
+            if j % 2 == 0:
                     rank, _ = get_dist_info()
                     if rank == 0:
                         if args.out:
