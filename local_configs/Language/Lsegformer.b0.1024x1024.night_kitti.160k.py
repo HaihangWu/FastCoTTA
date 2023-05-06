48bd845a5fba67c22efdd4518ed1d4f33de8f624
hthh1211 <hthh1211@sina.com>
2023-05-06 21:41:33 +1000

    new commit

--- local_configs/Language/Lsegformer.b0.1024x1024.night_kitti.160k.py
+++ local_configs/Language/Lsegformer.b0.1024x1024.night_kitti.160k.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/segformer.py',
-    '../_base_/datasets/night_cityscapes_1024x1024_repeat.py',
+    '../_base_/datasets/night_kitti_step_1024x1024_repeat.py',
     '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_160k_adamw.py'
 ]
