60e09f3a5c478d8c870fc229f215bc463a313ecc
hthh1211 <hthh1211@sina.com>
2023-05-07 20:45:36 +1000

    new commit

--- tools/cotta.py
+++ tools/cotta.py
@@ -118,13 +118,13 @@ def main():
     #if args.aug_test:
     if True:
         for i in range(len(cfg.data.test.test_cases)):
-            if cfg.data.test.test_cases[i].type in ['CityscapesDataset', 'ACDCDataset','KITTIDataset']:
+            if cfg.data.test.test_cases[i].type in ['CityscapesDataset', 'ACDCDataset','KITTIDataset','NightCityDataset']:
                 # hard code index
                 cfg.data.test.test_cases[i].pipeline[1].img_ratios = [
                     0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0
                     #1.0, 2.0
                 ]
-                cfg.data.test.test_cases[i].pipeline[1].flip = False
+                cfg.data.test.test_cases[i].pipeline[1].flip = True
             elif cfg.data.test.test_cases[i].type == 'ADE20KDataset':
                 # hard code index
                 cfg.data.test.test_cases[i].pipeline[1].img_ratios = [
