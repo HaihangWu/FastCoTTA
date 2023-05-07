4a5598d40958033b457b0a71be56a6ca0c4f0935
hthh1211 <hthh1211@sina.com>
2023-05-07 15:59:19 +1000

    new commit

---
+++ local_configs/Language/Lsegformer.b3.1024x1024.night_kitti.160k.py
@@ -0,0 +1,75 @@
+_base_ = [
+    '../_base_/models/segformer.py',
+    '../_base_/datasets/night_kitti_step_1024x1024_repeat.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_160k_adamw.py'
+]
+
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+find_unused_parameters = True
+model = dict(
+    type='SegLanguage',
+    #pretrained='../mmsegmentation/segformer.b3.1024x1024.city.160k.pth',
+    pretrained='work_dirs/Lsegformer.b3.1024x1024.city.160k/iter_16000.pth',
+    backbone=dict(
+        type='mit_b3',
+        style='pytorch'),
+    decode_head=dict(
+        type='SegFormerHead',
+        in_channels=[64, 128, 320, 512],
+        in_index=[0, 1, 2, 3],
+        feature_strides=[4, 8, 16, 32],
+        channels=128,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        decoder_params=dict(embed_dim=768),
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    text_encoder=dict(
+        type='CLIPTextEncoder',
+        pretrained='pretrained/ViT-B-16.pt',
+        context_length=77,
+        embed_dim=512,
+        transformer_width=512,
+        transformer_heads=8,
+        transformer_layers=12,
+        style='pytorch'),
+    text_decoder=dict(
+        type='TextSegFormerHead',
+        in_channels=[64, 128, 320, 512],
+        in_index=[0, 1, 2, 3],
+        feature_strides=[4, 8, 16, 32],
+        channels=128,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        decoder_params=dict(embed_dim=512),
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    # model training and testing settings
+    train_cfg=dict(),
+    # test_cfg=dict(mode='whole'))
+    test_cfg=dict(mode='slide', crop_size=(1024,1024), stride=(768,768)),
+    ft_model=True,
+    include_key='linear_pred',
+    # load_text_embedding='configs/_base_/datasets/text_embedding/voc12_single.npy'
+)
+
+# data
+data = dict(samples_per_gpu=1)
+evaluation = dict(interval=4000, metric='mIoU')
+
+# optimizer
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+                 paramwise_cfg=dict(custom_keys={'pos_block': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.),
+                                                 'head': dict(lr_mult=10.)
+                                                 }))
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
