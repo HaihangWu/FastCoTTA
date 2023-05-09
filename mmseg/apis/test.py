40fa739ef7105425465776e2cc89f1c9bc7d1377
hthh1211 <hthh1211@sina.com>
2023-05-09 21:51:31 +1000

    new

--- mmseg/apis/test.py
+++ mmseg/apis/test.py
@@ -400,7 +400,7 @@ def single_gpu_language_cotta(model,
         #             palette=dataset.PALETTE,
         #             show=show,
         #             out_file=out_file)
-        if (new_domain_frame+200)>frame_passed or round%1==0:
+        if (new_domain_frame+200)>frame_passed or round%2==1:
         #if adapt and (len(domains_detections["validation_frame"][0])==domains_detections["num_validation_frame"]):
             #model = deepcopy(ema_model)
             # for ema_param, param in zip(ema_model.parameters(), model.parameters()):
