64b8a561bd0b0fc9bebada6a313c5f3af336c2ba
hthh1211 <hthh1211@sina.com>
2023-05-11 02:03:22 +1000

    new

--- mmseg/apis/test.py
+++ mmseg/apis/test.py
@@ -335,7 +335,7 @@ def single_gpu_language_cotta(model,
                 cur_distri_std = np.std(cur_distribution)
                 last_distri_std = np.std(last_distribution)
                 wass_dist=wasserstein_distance(last_distribution,cur_distribution)
-                print("domain detection", wass_dist, last_distri_std, cur_distri_std,  frame_passed)
+                print("domain detection", last_mean, cur_mean, wass_dist, last_distri_std, cur_distri_std,  frame_passed)
                 if wass_dist>(last_distri_std) and not domains_detections["adaptation"]: #and (abs(cur_mean-last_mean)/np.sqrt(cur_distri_std**2.0+last_distri_std**2.0))>2.0:
                     domains_detections["adaptation"] = True
                     #domains_detections["validation_frame"] = [[],[]]
