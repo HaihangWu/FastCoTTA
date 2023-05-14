e501cacdd9a46bca1284b0967289bdb4901d6adb
hthh1211 <hthh1211@sina.com>
2023-05-14 21:03:54 +1000

    new commit

--- tools/dist_language_cotta.sh
+++ tools/dist_language_cotta.sh
@@ -14,4 +14,4 @@ model_name=$7
 PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4)) #wuhh
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/language_cotta.py $CONFIG --launcher pytorch --outlier_num outlier_num --z_score_threshold z_threshold --lang_rgz lang_rgz --adp_termination adp_termination --model_name model_name ${@:8}
+    $(dirname "$0")/language_cotta.py $CONFIG --launcher pytorch --outlier_num $outlier_num --z_score_threshold $z_threshold --lang_rgz $lang_rgz --adp_termination $adp_termination --model_name $model_name ${@:8}
