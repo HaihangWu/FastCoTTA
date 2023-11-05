22bc44e79a802d4746a89ac244f08f5d02fde764
hthh1211 <hthh1211@sina.com>
2023-11-05 23:55:44 +1100

    new commit

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -2,9 +2,9 @@
 
 CONFIG=$1
 #CHECKPOINT=$2
-GPUS=$2
+GPUS=$3
 #PORT=${PORT:-29505}
 PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4))
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/language_test.py $CONFIG --launcher pytorch ${@:3}
+    $(dirname "$0")/test_old.py $CONFIG --launcher pytorch ${@:3}
