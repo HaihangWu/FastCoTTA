bc51775861d26dd6aa1830e3111c58860ea47988
hthh1211 <hthh1211@sina.com>
2023-11-06 12:05:17 +1100

    new commit

--- tools/test_old.py
+++ tools/test_old.py
@@ -7,7 +7,7 @@ from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import get_dist_info, init_dist, load_checkpoint
 from mmcv.utils import DictAction
 
-from mmseg.apis import multi_gpu_test, single_gpu_test
+from mmseg.apis import single_gpu_test
 from mmseg.datasets import build_dataloader, build_dataset
 from mmseg.models import build_segmentor
 from IPython import embed
