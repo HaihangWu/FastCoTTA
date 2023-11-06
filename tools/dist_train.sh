2a077b44f2ea69e1ce409cc2fc5e83c1985e3e8a
hthh1211 <hthh1211@sina.com>
2023-11-06 22:26:12 +1100

    new commit

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 CONFIG=$1
-#CHECKPOINT=$2
-GPUS=$2
+WorkDir=$2
+GPUS=$3
 #PORT=${PORT:-29505}
 PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4))
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/language_train.py $CONFIG --launcher pytorch ${@:3}
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --work-dir $WorkDir  ${@:4}
