871feeb06fcfcf656a22a499143c51c7ff625424
hthh1211 <hthh1211@sina.com>
2023-11-06 22:11:59 +1100

    new commit

--- tools/dist_cotta.sh
+++ tools/dist_cotta.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 CONFIG=$1
-#CHECKPOINT=$2
-GPUS=$2
+Method=$2
+GPUS=$3
 #PORT=${PORT:-29505}
 PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4))
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/cotta.py $CONFIG --launcher pytorch ${@:3}
+    $(dirname "$0")/cotta.py $CONFIG  --launcher pytorch --method $Method ${@:4}
