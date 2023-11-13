fd30e0b2278a9a5924c51285e05212bf8f372f52
hthh1211 <hthh1211@sina.com>
2023-11-14 00:54:19 +1100

    new commit

--- local_configs/ours/segnext.base.1024x1024.city.160k.py
+++ local_configs/ours/segnext.base.1024x1024.city.160k.py
@@ -37,8 +37,8 @@ model = dict(
 
 # data
 data = dict(samples_per_gpu=6)
-# evaluation = dict(interval=100, metric='mIoU')
-checkpoint_config = dict(by_epoch=False, interval=200)
+evaluation = dict(interval=170000, metric='mIoU')
+checkpoint_config = dict(by_epoch=False, interval=500)
 # optimizer
 optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
                  paramwise_cfg=dict(custom_keys={'pos_block': dict(decay_mult=0.),
