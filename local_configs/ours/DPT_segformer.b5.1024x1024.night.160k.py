2f25492f655c92f965c92382dc011a9cf119061d
hthh1211 <hthh1211@sina.com>
2023-11-15 12:21:03 +1100

    new commit

--- local_configs/ours/DPT_segformer.b5.1024x1024.night.160k.py
+++ local_configs/ours/DPT_segformer.b5.1024x1024.night.160k.py
@@ -7,7 +7,7 @@ _base_ = [
 
 # model settings
 norm_cfg = dict(type='SyncBN', requires_grad=True)
-prompt_config=dict(NUM_TOKENS = 100,LOCATION = "random")
+prompt_config=dict(NUM_TOKENS = 2500,LOCATION = "random")
 find_unused_parameters = True
 model = dict(
     type='EncoderDecoder',
