79d9b05b562eee582b93fcf057db68f56384919d
hthh1211 <hthh1211@sina.com>
2023-11-16 11:58:01 +1100

    new commit

--- local_configs/ours/segformer.b5.1024x1024.city.160k.py
+++ local_configs/ours/segformer.b5.1024x1024.city.160k.py
@@ -39,11 +39,11 @@ model = dict(
 )
 
 # data
-data = dict(samples_per_gpu=1)
+data = dict(samples_per_gpu=6)
 evaluation = dict(interval=170000, metric='mIoU')
 checkpoint_config = dict(by_epoch=False, interval=2000)
 # optimizer
-optimizer = dict(_delete_=True, type='AdamW', lr=0.00006/6, betas=(0.9, 0.999), weight_decay=0.01,
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00006/8*6, betas=(0.9, 0.999), weight_decay=0.01,
                  paramwise_cfg=dict(custom_keys={'pos_block': dict(decay_mult=0.),
                                                  'norm': dict(decay_mult=0.),
                                                  'head': dict(lr_mult=10.)
