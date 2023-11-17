b7e6ab14a4d69e8e83e34a5678d973bb09e93366
hthh1211 <hthh1211@sina.com>
2023-11-18 01:07:30 +1100

    new commit

---
+++ imagenet/dist_cotta.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#CONFIG=$1
+#Method=$2
+#GPUS=$3
+#PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4))
+#PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+#CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#    $(dirname "$0")/cotta.py $CONFIG  --launcher pytorch --method $Method ${@:4}
+
+for i in {0..9}
+do
+#    CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/10orders/tent/tent$i.yaml
+    CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/10orders/cotta/cotta$i.yaml
+#    CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/10orders/fastcotta/cotta$i.yaml
+done
+# Run Mean and AVG for TENT, CoTTA
+cd output
+python3 -u ../eval.py | tee result.log
--- imagenet/run.sh
+++ imagenet/run.sh
@@ -1,11 +1,11 @@
 #source /cluster/home/qwang/miniconda3/etc/profile.d/conda.sh
 # Clean PATH and only use cotta env
-export PYTHONPATH=
-conda deactivate
-conda activate cotta
-# Source-only and AdaBN results are not affected by the order as no training is performed. Therefore only need to run once.
-CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/source.yaml 
-CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/norm.yaml
+#export PYTHONPATH=
+#conda deactivate
+#conda activate cotta
+## Source-only and AdaBN results are not affected by the order as no training is performed. Therefore only need to run once.
+#CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/source.yaml
+#CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/norm.yaml
 # TENT and CoTTA results are affected by the corruption sequence order
 for i in {0..9}
 do
