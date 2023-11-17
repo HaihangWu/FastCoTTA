b91d9aee33d379a4d6e19b411c80d888957b4152
hthh1211 <hthh1211@sina.com>
2023-11-18 03:35:54 +1100

    new commit

--- imagenet/dist_cotta.sh
+++ imagenet/dist_cotta.sh
@@ -8,6 +8,8 @@
 #CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
 #    $(dirname "$0")/cotta.py $CONFIG  --launcher pytorch --method $Method ${@:4}
 
+cd imagenet
+
 CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/source.yaml
 CUDA_VISIBLE_DEVICES=0 python -u imagenetc.py --cfg cfgs/norm.yaml
 
