0b5c9293030aaf5eef0f14efa5dbe70790a88177
hthh1211 <hthh1211@sina.com>
2024-04-06 20:46:10 +1100

    new

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -1,6 +1,4 @@
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
-from .seg_language import SegLanguage
-from .setr_language import SETRLanguage
 
-__all__ = ['EncoderDecoder', 'CascadeEncoderDecoder','SegLanguage','SETRLanguage']
+__all__ = ['EncoderDecoder', 'CascadeEncoderDecoder']
