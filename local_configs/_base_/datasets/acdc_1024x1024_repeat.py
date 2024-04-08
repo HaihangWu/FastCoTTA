a428e1812df757099fc5331a3ba6ba37014c97d8
hthh1211 <hthh1211@sina.com>
2024-04-08 23:07:54 +1000

    new

--- local_configs/_base_/datasets/acdc_1024x1024_repeat.py
+++ local_configs/_base_/datasets/acdc_1024x1024_repeat.py
@@ -1,7 +1,7 @@
 # dataset settings
 dataset_type = 'ACDCDataset'
-#data_root = '/data/gpfs/projects/punim0512/data/data_acdc/'
-data_root =  '/data/gpfs/projects/punim0512/data/test/acdc/'
+data_root = '/data/gpfs/projects/punim0512/data/data_acdc/'
+#data_root =  '/data/gpfs/projects/punim0512/data/test/acdc/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (1024, 1024)
