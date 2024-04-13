3522e2f218b79da83c272f4561aad418cc355d2a
hthh1211 <hthh1211@sina.com>
2024-04-13 23:21:22 +1000

    new

--- cifar100/OSTTA.py
+++ cifar100/OSTTA.py
@@ -59,7 +59,7 @@ def forward_and_adapt(self, x, model, optimizer):
     outputs_prob_at_anchor = outputs_prob_dist[torch.arange(outputs_prob_dist.size(0)), anchor_class]
     Mask = (outputs_prob_at_anchor >= anchor_prob).float()
     # adapt
-    loss = (softmax_entropy(outputs) * 1.0).mean(0)
+    loss = (softmax_entropy(outputs) * Mask).mean(0)
     # print(softmax_entropy(outputs))
     # print(softmax_entropy(outputs)*Mask)
     loss.backward()
