1d3e8977d92e03c155b37d8cfcdbe876cf84798a
hthh1211 <hthh1211@sina.com>
2024-04-16 22:13:14 +1000

    new

--- imagenet/dist_cotta.sh
+++ imagenet/dist_cotta.sh
@@ -14,13 +14,13 @@ cd imagenet
 for i in {0..0}
 do
   CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/source.yaml
-#  CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/norm.yaml
-#  CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/tent.yaml
-#  CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/cotta.yaml
-#  CUDA_VISIBLE_DEVICES=0 python  imagenetc.py --cfg cfgs/imagenet/ETA.yaml
+  CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/norm.yaml
+  CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/tent.yaml
+  CUDA_VISIBLE_DEVICES=0 python  imagenetc.py --cfg cfgs/imagenet/ETA.yaml
 #  CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/rdumb.yaml
 #  CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/OSTTA.yaml
-#   CUDA_VISIBLE_DEVICES=0 python  imagenetc.py --cfg cfgs/imagenet/fastcotta.yaml
+   CUDA_VISIBLE_DEVICES=0 python  imagenetc.py --cfg cfgs/imagenet/fastcotta.yaml
+     CUDA_VISIBLE_DEVICES=0 python imagenetc.py --cfg cfgs/imagenet/cotta.yaml
 
 done
 
