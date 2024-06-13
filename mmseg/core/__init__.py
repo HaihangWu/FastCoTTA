ec1e0ffa19eddb85d72588b9ac55c057b0bd676f
hthh1211 <hthh1211@sina.com>
2024-06-13 16:51:28 +1000

    new

--- mmseg/core/__init__.py
+++ mmseg/core/__init__.py
@@ -2,5 +2,5 @@ from .evaluation import *  # noqa: F401, F403
 from .seg import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
 
-from .builder import (OPTIMIZER_BUILDERS, build_optimizer,
-                      build_optimizer_constructor)
+# from .builder import (OPTIMIZER_BUILDERS, build_optimizer,
+#                       build_optimizer_constructor)
