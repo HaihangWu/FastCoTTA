aa626b387cb0b2c2f7ec74a14d280406f3c5492e
hthh1211 <hthh1211@sina.com>
2024-06-13 20:29:29 +1000

    new

--- mmseg/__init__.py
+++ mmseg/__init__.py
@@ -4,8 +4,8 @@ from .version import __version__, version_info
 
 # MMCV_MIN = '1.1.4'
 # MMCV_MAX = '1.3.0'
-MMCV_MIN = '1.2.7'
-MMCV_MAX = '2.2.0'
+MMCV_MIN = '1.3.13'
+MMCV_MAX = '1.8.0'
 
 
 def digit_version(version_str):
