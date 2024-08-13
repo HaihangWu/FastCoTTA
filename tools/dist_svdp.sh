f8ea003e1d4043ebb8e036b403fa4d219c4a984c
hthh1211 <hthh1211@sina.com>
2024-08-13 21:02:27 +1000

    new

--- tools/dist_svdp.sh
+++ tools/dist_svdp.sh
@@ -7,4 +7,4 @@ GPUS=$3
 PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4))
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/cotta.py $CONFIG  --launcher pytorch --ema_rate=0.999 --model_lr=0.0001 --prompt_lr=0.0001 --prompt_sparse_rate=0.001 --scale=0.1
+    $(dirname "$0")/svdp.py $CONFIG  --launcher pytorch --ema_rate=0.999 --model_lr=0.0001 --prompt_lr=0.0001 --prompt_sparse_rate=0.001 --scale=0.1
