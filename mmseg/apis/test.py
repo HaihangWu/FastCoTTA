bbdcf2a08cee6a440b976724537968c98c567513
hthh1211 <hthh1211@sina.com>
2024-08-13 20:50:48 +1000

    new

--- mmseg/apis/test.py
+++ mmseg/apis/test.py
@@ -9,6 +9,7 @@ import numpy as np
 import torch
 import torch.distributed as dist
 from torch.distributions import Categorical
+import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.image import tensor2imgs
 from mmcv.runner import get_dist_info
@@ -23,7 +24,8 @@ import copy
 import math
 import torchvision.transforms.functional as TF
 from PIL import Image
-
+from mmseg.models.backbones.prompt import *
+from mmseg.models.backbones.lora import *
 
 def update_ema_variables(ema_model, model, alpha_teacher, iteration=None):
     # Use the "true" average until the exponential average is more correct
@@ -34,6 +36,20 @@ def update_ema_variables(ema_model, model, alpha_teacher, iteration=None):
         for ema_param, param in zip(ema_model.parameters(), model.parameters()):
             #ema_param.data.mul_(alpha).add_(1 - alpha, param.data)
             ema_param.data[:] = alpha_teacher * ema_param[:].data[:] + (1 - alpha_teacher) * param[:].data[:]
+    return ema_model
+
+def update_ema_variables_svdp(ema_model, model, alpha_teacher, alpha_prompt, iteration=None):
+    # Use the "true" average until the exponential average is more correct
+    if iteration:
+        alpha_teacher = min(1 - 1 / (iteration + 1), alpha_teacher)
+
+    if True:
+        for ema_param, (name, param) in zip(ema_model.parameters(), model.named_parameters()):
+            #ema_param.data.mul_(alpha).add_(1 - alpha, param.data)
+            if "prompt" in name:
+                ema_param.data[:] = alpha_prompt * ema_param[:].data[:] + (1 - alpha_prompt) * param[:].data[:]
+            else:
+                ema_param.data[:] = alpha_teacher * ema_param[:].data[:] + (1 - alpha_teacher) * param[:].data[:]
     return ema_model
 
 def np2tmp(array, temp_file_name=None):
@@ -54,6 +70,10 @@ def np2tmp(array, temp_file_name=None):
             suffix='.npy', delete=False).name
     np.save(temp_file_name, array)
     return temp_file_name
+
+def softmax_entropy(x: torch.Tensor) -> torch.Tensor:
+    """Entropy of softmax distribution from logits."""
+    return -(x.softmax(1) * x.log_softmax(1)).sum(1)
 
 def single_gpu_ours(model,
                     data_loader,
@@ -235,6 +255,122 @@ def single_gpu_ours(model,
     print("average pred_time: %.3f seconds;" % (pred_time/(i+1)))
     #print("student model confidence: %.3f" % ())
     return results,frame_passed,domains_detections
+
+def single_gpu_svdp(args,
+                    model,
+                    data_loader,
+                    show=False,
+                    out_dir=None,
+                    efficient_test=False,
+                    anchor=None,
+                    ema_model=None,
+                    anchor_model=None,
+                    dynamic_ema=False,
+                    dynamic_prompt_only=False,
+                   ):
+    """Test with single GPU.
+
+    Args:
+        model (nn.Module): Model to be tested.
+        data_loader (utils.data.Dataloader): Pytorch data loader.
+        show (bool): Whether show results during infernece. Default: False.
+        out_dir (str, optional): If specified, the results will be dumped into
+            the directory to save output results.
+        efficient_test (bool): Whether save the results as local numpy files to
+            save CPU memory during evaluation. Default: False.
+
+    Returns:
+        list: The prediction results.
+    """
+    # model.eval()
+    anchor_model.eval()
+    results = []
+    dataset = data_loader.dataset
+    prog_bar = mmcv.ProgressBar(len(dataset))
+    param_model_list = []
+    param_prompt_list = []
+
+    for name, param in model.named_parameters():
+        if param.requires_grad and "prompt" in name:
+            param_prompt_list.append(param)
+        elif param.requires_grad and "prompt" not in name:
+            param_model_list.append(param)
+        else:
+            param.requires_grad=False
+    optimizer = torch.optim.Adam([{"params": param_prompt_list, "lr": args.prompt_lr},
+                                  {"params": param_model_list, "lr": args.model_lr}],
+                                 lr=1e-5, betas=(0.9, 0.999))
+    for i, data in enumerate(data_loader):
+        model.eval()
+        ema_model.eval()
+        ema_model.module.decode_head.dropout = nn.Dropout(0.5)
+        ema_model.module.decode_head.dropout.train()
+        ema_model.module.backbone.prompt.if_mask = False
+
+        with torch.no_grad():
+            data_one = {
+                'img_metas': [data['img_metas'][4] for i in range(1)],
+                'img': [data['img'][4] for i in range(1)]
+            }
+            _, _, _, unc_all = ema_model(return_loss=False, svdp = True, dropout_num=10, **data_one)
+
+            _, prob_anchor= anchor_model(return_loss=False, **data_one)
+
+            mask = (prob_anchor[0] > 0.69).astype(np.int64) # 0.74 was the 5% quantile for cityscapes, therefore we use 0.69 here
+
+            variance = np.var(unc_all, axis=0)
+            uncertainty = np.sum(variance, axis=1)
+            # Domain Prompt Placement
+            model.module.backbone.prompt.if_mask = True
+            model.module.backbone.prompt.update_uncmap(uncertainty[0])
+            model.module.backbone.prompt.update_mask()
+
+            ema_model.module.backbone.prompt.if_mask = True
+            ema_model.module.backbone.prompt.update_uncmap(uncertainty[0])
+            ema_model.module.backbone.prompt.update_mask()
+            ema_model.eval()
+
+            result, probs, preds = ema_model(return_loss=False,  **data)
+
+            result = [(mask*preds[4][0] + (1.-mask)*result[0]).astype(np.int64)]
+            weight = 1.
+
+        if isinstance(result, list):
+            if len(data['img']) == 14:
+                img_id = 4 #The default size without flip
+            else:
+                img_id = 0
+
+            loss = model.forward(return_loss=True, img=data['img'][img_id], img_metas=data['img_metas'][img_id].data[0], gt_semantic_seg=torch.from_numpy(result[0]).cuda().unsqueeze(0).unsqueeze(0))
+            if efficient_test:
+                result = [np2tmp(_) for _ in result]
+            results.extend(result)
+        else:
+            if efficient_test:
+                result = np2tmp(result)
+            results.append(result)
+
+        torch.mean(weight*loss["decode.loss_seg"]).backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        # Domain Prompt Updating
+        prompt_rate = args.ema_rate - np.average(uncertainty) * args.scale
+
+        ema_model = update_ema_variables_svdp(ema_model = ema_model, model = model, alpha_model=args.ema_rate, alpha_prompt = prompt_rate)
+        for nm, m in model.named_modules():
+            for npp, p in m.named_parameters():
+                if npp in ['weight', 'bias'] and p.requires_grad:
+                    mask = (torch.rand(p.shape)<0.01).float().cuda()
+                    with torch.no_grad():
+                        p.data = anchor[f"{nm}.{npp}"] * mask + p * (1.-mask)
+
+        batch_size = data['img'][0].size(0)
+        for _ in range(batch_size):
+            prog_bar.update()
+
+    return results
+
+
 
 def single_gpu_cotta(model,
                     data_loader,
