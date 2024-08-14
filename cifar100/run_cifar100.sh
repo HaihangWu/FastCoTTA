0a6f04f13cf94a9b390516795f0f4746a8a7e8df
hthh1211 <hthh1211@sina.com>
2024-08-14 17:04:24 +1000

    new

--- cifar100/run_cifar100.sh
+++ cifar100/run_cifar100.sh
@@ -1,10 +1,12 @@
 #source /cluster/home/qwang/miniconda3/etc/profile.d/conda.sh
-export PYTHONPATH= 
-conda deactivate
-conda activate cotta 
-CUDA_VISIBLE_DEVICES=0 python cifar100c.py --cfg cfgs/cifar100/source.yaml
-CUDA_VISIBLE_DEVICES=0 python cifar100c.py --cfg cfgs/cifar100/norm.yaml
-CUDA_VISIBLE_DEVICES=0 python cifar100c.py --cfg cfgs/cifar100/tent.yaml
-CUDA_VISIBLE_DEVICES=0 python cifar100c.py --cfg cfgs/cifar100/cotta.yaml
+#export PYTHONPATH=
+#conda deactivate
+#conda activate cotta
+#CUDA_VISIBLE_DEVICES=0 python cifar100c.py --cfg cfgs/cifar100/source.yaml
+#CUDA_VISIBLE_DEVICES=0 python cifar100c.py --cfg cfgs/cifar100/norm.yaml
+#CUDA_VISIBLE_DEVICES=0 python cifar100c.py --cfg cfgs/cifar100/tent.yaml
+#CUDA_VISIBLE_DEVICES=0 python cifar100c.py --cfg cfgs/cifar100/cotta.yaml
 
 
+CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar100/sar.yaml
+CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar100/fastcotta.yaml
