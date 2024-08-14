598e97883617ea443c49e725d40cb4db34292f05
hthh1211 <hthh1211@sina.com>
2024-08-14 15:07:09 +1000

    new

--- cifar10/run_cifar10.sh
+++ cifar10/run_cifar10.sh
@@ -8,8 +8,6 @@
 #CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/cotta.yaml
 
 
-PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4))
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/sar.yaml
 
 
