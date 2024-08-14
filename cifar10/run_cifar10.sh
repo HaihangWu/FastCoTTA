122aad526b221f62a4b9349298bd8c8bc3f95534
hthh1211 <hthh1211@sina.com>
2024-08-14 15:01:55 +1000

    new

--- cifar10/run_cifar10.sh
+++ cifar10/run_cifar10.sh
@@ -1,12 +1,15 @@
 #source /cluster/home/qwang/miniconda3/etc/profile.d/conda.sh
-export PYTHONPATH= 
-conda deactivate
-conda activate cotta
+#export PYTHONPATH=
+#conda deactivate
+#conda activate cotta
 #CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/source.yaml
 #CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/norm.yaml
 #CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/tent.yaml
 #CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/cotta.yaml
 
+
+PORT=$(expr 20000 + $(echo -n $SLURM_JOBID | tail -c 4))
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/sar.yaml
 
 
